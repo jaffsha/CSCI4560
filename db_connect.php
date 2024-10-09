80b0a5459d8341a192fdc9d3fc1845642bae633c
Jamie-Groves <jcg5v@mtmail.mtsu.edu>
2024-10-08 23:02:03 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  // Your server name
 $username = "root";         // Your MySQL username
 $password = "";             // Your MySQL password (default is empty for XAMPP)
-$dbname = "library_management"; // Your database name
+$dbname = "library_database"; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
